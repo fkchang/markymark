38e826c702c31c74076dc9fe2d790a8a6facd07f
Forrest Chang <fchang@hedgeye.com>
2025-11-18 13:03:34 -0800

    Remove placeholder test from spec
    - Removed intentionally failing placeholder test
    - All specs now pass cleanly (1 example, 0 failures)

--- spec/markymark_spec.rb
+++ spec/markymark_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Markymark do
   it "has a version number" do
     expect(Markymark::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
